6295fba6c9e164c3a0119cff588da995807a139d
Preta3418 <jason123qwe@gmail.com>
2025-11-14 14:56:18 +0900

    Feat: Phone Number Converter in MessageService implemented
    Converter to change the phone numbers to e.164 format. It throws the runtime exception when number is not found, but this will almost never happen so...

--- src/main/java/com/judtih/judith_management_system/domain/message/MessageService.java
+++ src/main/java/com/judtih/judith_management_system/domain/message/MessageService.java
@@ -1,9 +1,12 @@
 package com.judtih.judith_management_system.domain.message;
 
+import com.judtih.judith_management_system.domain.user.User;
 import com.judtih.judith_management_system.domain.user.UserService;
 import com.judtih.judith_management_system.shared.config.AwsConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.SplittableRandom;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +16,19 @@ public class MessageService {
     UserService userService;
 
 
+protected String phoneNumberConverter (User user) {
+    String phoneNum = user.getPhoneNumber();
 
+    if (phoneNum == null) {
+        throw new RuntimeException("no phone number found for user : " + user.getName());
+    } else if (phoneNum.startsWith("+82")) {
+        return phoneNum;
+    }
+    phoneNum = phoneNum.replace("-", "");
+    phoneNum = phoneNum.substring(1);
+    phoneNum = "+82" + phoneNum;
+
+    return phoneNum;
+}
 
 }
