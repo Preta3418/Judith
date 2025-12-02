8cfa6627ee57866a84f1dadec90dc8c4a817b68c
Preta3418 <jason123qwe@gmail.com>
2025-12-02 16:43:21 +0900

    fix: made no and all args constructor for the message result dto. Will need it later for Jackson

--- src/main/java/com/judtih/judith_management_system/domain/message/dto/MessageResult.java
+++ src/main/java/com/judtih/judith_management_system/domain/message/dto/MessageResult.java
@@ -2,12 +2,17 @@ package com.judtih.judith_management_system.domain.message.dto;
 
 
 import com.judtih.judith_management_system.domain.message.entity.MessageFailure;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 //back end to front end
 public class MessageResult {
     int totalAttempted;
