e6a743d613dfea2022fe61a02a87dc04f2fa5f90
Preta3418 <jason123qwe@gmail.com>
2025-12-02 16:35:33 +0900

    feat: EventRepository and ReservationRepository made
    Created Event and Reservation Repository. For the racing conition, I decided to use the pessimistic lock on findById, so we can stop reservation problem. may change later on.

--- src/main/java/com/judtih/judith_management_system/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/judtih/judith_management_system/domain/reservation/repository/ReservationRepository.java
@@ -1,9 +1,29 @@
 package com.judtih.judith_management_system.domain.reservation.repository;
 
 import com.judtih.judith_management_system.domain.reservation.entity.Reservation;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Modifying
+    @Transactional
+    void deleteByEventId(Long eventId);
+
+    Optional<Reservation> findByEventIdAndPhoneNumber(Long eventId, String phoneNumber);
+
+    boolean existsByEventIdAndPhoneNumber(Long eventId, String PhoneNumber);
+
+    List<Reservation> findByEventId(Long eventId);
+
+    @Query("SELECT COALESCE(SUM(r.ticketCount), 0) FROM Reservation r WHERE r.event.id = :eventId AND r.status = 'CONFIRMED'")
+    Integer sumConfirmedGuestsByEventId(@Param("eventId") Long eventId);
 }
