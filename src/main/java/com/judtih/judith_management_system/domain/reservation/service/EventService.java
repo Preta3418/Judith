1db0991aea36a18deff04d94432adfb8c9a0f3e5
Preta3418 <jason123qwe@gmail.com>
2025-12-05 14:28:39 +0900

    feat: added Event Service
    created eventService.it's a basic CRUD service so nothing too much to talk about here.

---
+++ src/main/java/com/judtih/judith_management_system/domain/reservation/service/EventService.java
@@ -0,0 +1,109 @@
+package com.judtih.judith_management_system.domain.reservation.service;
+
+import com.judtih.judith_management_system.domain.reservation.entity.Event;
+import com.judtih.judith_management_system.domain.reservation.eventDto.EventListResponse;
+import com.judtih.judith_management_system.domain.reservation.eventDto.EventRequest;
+import com.judtih.judith_management_system.domain.reservation.eventDto.EventResponse;
+import com.judtih.judith_management_system.domain.reservation.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+
+    //Admin methods ///////////////////////////////////////////////////////
+
+    public EventResponse createEvent(EventRequest eventRequest) {
+        Event event = Event.builder()
+                .title(eventRequest.getTitle())
+                .eventDate(eventRequest.getEventDate())
+                .description(eventRequest.getDescription())
+                .location(eventRequest.getLocation())
+                .capacityLimit(eventRequest.getCapacityLimit())
+                .registrationDeadline(eventRequest.getRegistrationDeadLine())
+                .status(eventRequest.getStatus())
+                .posterImageUrl(eventRequest.getPosterImageUrl())
+                .build();
+
+        eventRepository.save(event);
+
+        return createEventResponse(event);
+
+    }
+
+
+    public EventResponse updateEvent(Long id, EventRequest request) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Event not found"));
+
+        event.eventUpdate(request.getTitle(),request.getDescription()
+                ,request.getEventDate(),request.getLocation(),request.getCapacityLimit()
+                ,request.getRegistrationDeadLine(),request.getStatus(),request.getPosterImageUrl());
+
+        eventRepository.save(event);
+
+        return createEventResponse(event);
+    }
+
+    public void deleteEventById(Long id) {
+        eventRepository.deleteById(id);
+
+    }
+
+    //User method /////////////////////////////////////////////////////////
+
+    public EventResponse getEventById(Long id) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("event not found"));
+
+        return createEventResponse(event);
+    }
+
+    public List<EventListResponse> getAllEvent() {
+        List<Event> events = eventRepository.findAll();
+        List<EventListResponse> responseList = new ArrayList<>();
+
+        for(Event event : events) {
+            EventListResponse response = createEventListResponse(event);
+
+            responseList.add(response);
+        }
+
+        return responseList;
+    }
+
+    //Helper method ///////////////////////////////////////////////////////
+
+    private EventResponse createEventResponse(Event event) {
+
+        return EventResponse.builder()
+                .id(event.getId())
+                .title(event.getTitle())
+                .description(event.getDescription())
+                .eventDate(event.getEventDate())
+                .location(event.getLocation())
+                .capacityLimit(event.getCapacityLimit())
+                .registrationDeadLine(event.getRegistrationDeadline())
+                .status(event.getStatus())
+                .posterImageUrl((event.getPosterImageUrl()))
+                .createdAt(event.getCreatedAt())
+                .build();
+    }
+
+    private EventListResponse createEventListResponse(Event event) {
+        return EventListResponse.builder()
+                .id(event.getId())
+                .title(event.getTitle())
+                .eventDate(event.getEventDate())
+                .posterImageUrl(event.getPosterImageUrl())
+                .status(event.getStatus())
+                .build();
+    }
+}
